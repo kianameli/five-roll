13688f1a81ce1867ff2416a126f35874af78f613
Kian Ameli <kianameli@Kians-MBP.attlocal.net>
2021-09-20 15:50:07 -0400

    fix .env.development.local

--- client/src/components/RecentScores.jsx
+++ client/src/components/RecentScores.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import axios from "axios";
+
+const airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
+const airtableKey = process.env.REACT_APP_AIRTABLE_KEY;
+const URL = `https://api.airtable.com/v0/${airtableBase}/recentScores`;
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${airtableKey}`,
+  },
+};
 
 export default function RecentScores() {
+
+  async function fetchScores() {
+    console.log(airtableBase,airtableKey);
+    const res = await axios.get(URL, config);
+    console.log(res);
+  }
+
+  fetchScores();
+  
+  
   return (
     <div>
       RECENT SCORES
